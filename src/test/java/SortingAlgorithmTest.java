ab311cbccfbc9d7c106359face87be7b7ae7cf3a
Wojciech Musiał <wojciech_musial@poczta.fm>
2019-02-23 12:20:59 +0100

    cleaning

--- src/test/java/SortingAlgorithmTest.java
+++ src/test/java/SortingAlgorithmTest.java
@@ -7,7 +7,7 @@ public class SortingAlgorithmTest {
     private SortingAlgorithms classUnderTest;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         classUnderTest = new SortingAlgorithms();
     }
 
@@ -20,13 +20,6 @@ public class SortingAlgorithmTest {
 
         Assert.assertArrayEquals(expected, toSort, 0.001);
     }
-    @Test
-    public void testSortowanieTO1(){
-        double[] toSort={1,4,5,3,2};
-        double[] expected={1,2,3,4,5};
-        classUnderTest.sort(toSort,true);
-        Assert.assertArrayEquals(expected,toSort,0.001);
-    }
 
     @Test
     public void testSortowaniaWM2() {
@@ -37,4 +30,13 @@ public class SortingAlgorithmTest {
 
         Assert.assertArrayEquals(expected, toSort, 0.001);
     }
+
+    @Test
+    public void testSortowanieTO1() {
+        double[] toSort = {1, 4, 5, 3, 2};
+        double[] expected = {1, 2, 3, 4, 5};
+        classUnderTest.sort(toSort, true);
+        Assert.assertArrayEquals(expected, toSort, 0.001);
+    }
+
 }
