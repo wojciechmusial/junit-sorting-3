f1a2207cd1236f272bcd67e10eef241a0cb788ac
Wojciech Musiał <wojciech_musial@poczta.fm>
2019-02-23 11:49:23 +0100

    test WM

--- src/test/java/SortingAlgorithmTest.java
+++ src/test/java/SortingAlgorithmTest.java
@@ -1,3 +1,23 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class SortingAlgorithmTest {
 
+    private SortingAlgorithms classUnderTest;
+
+    @Before
+    public void setUp() throws Exception {
+        classUnderTest = new SortingAlgorithms();
+    }
+
+    @Test
+    public void testSortowaniaWM1() {
+        double[] toSort = {5, 1, 3, 2};
+        double[] expected = {1, 2, 3, 5};
+
+        classUnderTest.sort(toSort, true);
+
+        Assert.assertArrayEquals(expected, toSort, 0.001);
+    }
 }
