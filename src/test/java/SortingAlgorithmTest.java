f125add4b55b40811a1fa8048bfd730fd4325be9
Tomasz O <amstafik81@gmail.com>
2019-02-23 11:55:49 +0100

    sortowanie TO

--- src/test/java/SortingAlgorithmTest.java
+++ src/test/java/SortingAlgorithmTest.java
@@ -20,4 +20,11 @@ public class SortingAlgorithmTest {
 
         Assert.assertArrayEquals(expected, toSort, 0.001);
     }
+    @Test
+    public void testSortowanieTO1(){
+        double[] toSort={1,4,5,3,2};
+        double[] expected={1,2,3,4,5};
+        classUnderTest.sort(toSort,true);
+        Assert.assertArrayEquals(expected,toSort,0.001);
+    }
 }
